ae94a392d01be31e9b4629eb152538b4854fff1b
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-07 21:57:56 +0300

    Copypaste the build scripts from https://github.com/alexanderfefelov/docker-mariadb-replication

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo --preserve-env=VERSION bash "$0" "$@"
+
+. settings.sh
+
+docker build \
+  --build-arg VERSION \
+  --tag $IMAGE_NAME:$VERSION \
+  .
+
+if [ "$VERSION" = "$DEFAULT_VERSION" ]; then
+  docker tag $IMAGE_NAME:$VERSION $IMAGE_NAME:latest
+fi
---
+++ settings.sh
@@ -0,0 +1,4 @@
+readonly DEFAULT_VERSION=8.0.23
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
+IMAGE_NAME=quay.io/alexanderfefelov/mysql-replication
